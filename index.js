cceea4e118b549f3c4c9d6fc855c73d01a28f431
jgumerove <josh.gumerove@gmail.com>
2021-08-24 17:33:28 -0400

    commit tree 03fdd687a67a9c8fd51f0bf493fb7ce8a78be89c

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    const newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+      newArray.push(sourceArray[i] * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    const newArray = sourceArray.slice()
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    const newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+      newArray.push(sourceArray[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    const newArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+      newArray.push(sourceArray[i] ** 2)
+    }
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+let total = startingPoint
+for (let i = 0; i < sourceArray.length; i++) {
+    total += sourceArray[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    let validator = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        validator.push(!!sourceArray[i])
+    }
+ if (validator.includes(false)) {
+     return false
+ } else {
+     return true 
+  }
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let validator = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        validator.push(!!sourceArray[i])
+    }
+ if (validator.includes(true)) {
+     return true
+ } else {
+     return false 
+  }
+}
